4dad177a0f6bc0594172b06da7e2638455f8ea08
spblue4422 <spblue4422@naver.com>
2022-01-18 00:57:17 +0900

    커밋

--- src/main/java/com/spblue4422/daangnclone/controller/PostController.java
+++ src/main/java/com/spblue4422/daangnclone/controller/PostController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class PostController {
     final PostService postService;
 
+    //post정보만 뽑아오는게 아니라 사진도 뽑아와야하고, 관심등록 여부도 확인해야함.
     public void getPostDetail(long postId) {
 
     }
